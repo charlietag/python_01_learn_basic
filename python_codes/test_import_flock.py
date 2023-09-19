6cfaa53f16f4a1020dab4e5cac80c2eb00efbabb
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-19 08:13:48 +0800

    test import class from __init__.py

---
+++ python_codes/test_import_flock.py
@@ -0,0 +1,3 @@
+import test_import_flock as f
+script = f.FileLock()
+script.lock()
---
+++ python_codes/test_import_flock/file_lock.py
@@ -0,0 +1,6 @@
+class FileLock:
+    def __init__(self, name="this.lock"):
+        print("init test")
+
+    def lock(self):
+        print("lock test")
