5844532631b940d4d28d6e7d68ca29b5d121a467
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-22 00:21:06 +0800

    finalize python lock class file, and add python package psutil for processID check

--- python_codes/test_import_flock.py
+++ python_codes/test_import_flock.py
@@ -1,6 +1,22 @@
-from pathlib import Path
 import test_import_flock as f
 
-lock_file = Path(__file__).with_suffix('.lock')
-script = f.FileLock(name=lock_file)
+# --- default in FileLock init ---
+# from pathlib import Path
+# lock_file = Path(__file__).with_suffix('.lock')
+# script = f.FileLock(name=lock_file)
+# --- default in FileLock init ---
+
+script = f.FileLock()
+
 script.lock()
+
+# -------------------------------------------------------
+# main
+# -------------------------------------------------------
+print("sleep 600")
+import time
+time.sleep(600)
+
+
+# -------------------------------------------------------
+script.unlock()
--- python_codes/test_import_flock/file_lock.py
+++ python_codes/test_import_flock/file_lock.py
@@ -1,7 +1,104 @@
-class FileLock:
-    def __init__(self, name="this.lock"):
-        print("init test")
-        print(name)
+# ------------------------------------------
+# --- Lock ---
+# fcntl.flock - only support linux
 
+# Ref. https://psutil.readthedocs.io/en/latest/
+# psutil - Cross platform
+#        - psutil.Process
+
+# --- check if variable is defined ---
+# Ref. https://www.geeksforgeeks.org/how-to-check-if-a-python-variable-exists/
+
+# locals()  - in function
+# globals() - outsidde function
+
+# ------------------------------------------
+
+from pathlib import Path
+import psutil
+
+class FileLock:
+    # ----------------------------------------
+    # init
+    # ----------------------------------------
+    def __init__(self, name = None):
+        if name is None:
+            import __main__
+            self._name = __main__.__file__
+        else:
+            self._name = name
+
+        self._f = Path(self._name)
+        self._lock_file = self._f.with_suffix('.lock')
+
+        # -------------------------------------
+        # true - test_import_flock.py exists
+        # print(self._f.is_file())
+
+        # false - test_import_flock.lock does not exists
+        # print(self._lock_file.is_file())
+        # -------------------------------------
+
+        print("init test")
+        # print(type(self._lock_file))
+        print(self._lock_file)
+
+    # ----------------------------------------
+    # lock
+    # ----------------------------------------
     def lock(self):
-        print("lock test")
+        self.check_process()
+        print("--- lock start ---")
+        current_pid = psutil.Process().pid
+
+        with open(self._lock_file, "w", encoding="utf-8") as f:
+            f.write(str(current_pid))
+
+        print("Current PID:", current_pid)
+        print(f"Current PID {current_pid} is written to {self._lock_file}")
+
+
+    # ----------------------------------------
+    # unlock
+    # ----------------------------------------
+    def unlock(self):
+        # no extra actions needed
+        print("--- unlock ---")
+        print("no extra actions needed")
+
+
+
+    # ----------------------------------------
+    # other method
+    # ----------------------------------------
+    def is_pid_running(self, pid = None):
+        try:
+            # Check if the process with the given PID is running
+            process = psutil.Process(pid)
+            return process.is_running()
+        except psutil.NoSuchProcess:
+            return False
+
+    def is_file_exists(self):
+        f_status = False
+
+        if self._lock_file.is_file():
+            f_status = True
+
+        return f_status
+
+
+    def check_process(self):
+        # print("check...")
+        # if self._lock_file.is_file():
+        if self.is_file_exists():
+            with open(self._lock_file, "r") as f:
+                pid = int(f.read().strip())
+
+            if self.is_pid_running(pid):
+                print(f"Process with PID {pid} is running.")
+                exit()
+        #     else:
+        #         print(f"Process with PID {pid} is not running.")
+        # else:
+        #     print("Lock file not found. The script may not be running.")
