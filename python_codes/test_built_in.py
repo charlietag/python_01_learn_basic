cf096562f1674c61275a522dcc478ecd29cea6c6
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-23 12:21:23 +0800

    try python built-in function

---
+++ python_codes/test_built_in.py
@@ -0,0 +1,67 @@
+# ------------------------------------------------
+# Try built-in
+# ------------------------------------------------
+my_list = []
+
+# ---------- range() ----------
+for i in range(10):
+    my_list.append(i)
+    print(i)
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+print(my_list)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# ---------- len() ----------
+print(len(my_list))
+# 10
+
+# ---------- min() ----------
+print(min(my_list))
+# 0
+
+# ---------- max() ----------
+print(max(my_list))
+# 9
+
+
+# ------------------------------------------------
+# Try built-in: list.sort(), and sorted(list)
+# ------------------------------------------------
+# Ref. https://docs.python.org/3/howto/sorting.html
+
+second_list = [6, 3,1, 2, 99, 88, 9, 8]
+
+print(second_list)
+# [6, 3, 1, 2, 99, 88, 9, 8]
+
+print(sorted(second_list))
+# [1, 2, 3, 6, 8, 9, 88, 99]
+
+print(second_list)
+# [6, 3, 1, 2, 99, 88, 9, 8]
+
+second_list.sort()
+print(second_list)
+# [1, 2, 3, 6, 8, 9, 88, 99]
+
+
+# ------------------------------------------------
+# Try built-in: strip()
+# ------------------------------------------------
+my_string = "     --- test string ---"
+print(my_string)
+#      --- test string ---
+
+print(my_string.strip())
+# --- test string ---
+
