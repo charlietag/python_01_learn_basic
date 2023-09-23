be628aa8b8415f79fe29f7c3c194e2ba00a05104
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-23 10:55:06 +0800

    add comment into python script

--- python_codes/01___list.py
+++ python_codes/01___list.py
@@ -1,39 +1,74 @@
+# -----------------------------------------------------
+# Test function
+# -----------------------------------------------------
+def print_help():
+    print("----------------------")
+    print("help function")
+    print("----------------------")
+
+def list_name():
+    print("----------------------")
+    for name in names:
+        show_name = "Welcome, " + str(name)
+        print(show_name)
+    print("----------------------")
+
+
+# -----------------------------------------------------
+# List
+# -----------------------------------------------------
 # this is just like array
 names = [
-    "Charlie",
-    "Welly",
+    "Bratte",
+    "Tom",
 ]
 
 print(len(names))
+# 2
+
 print(names[0])
+# Bratte
+
 print(names)
+# ['Bratte', 'Tom']
 
 names.append("Ohoh")
 print(names)
+# ['Bratte', 'Tom', 'Ohoh']
 
 # This will remove last one elememt "ohoh"
 names.pop()
 print(names)
+# ['Bratte', 'Tom']
 
-names.extend(["newGuy", "badGuy"])
+names.extend(["Gracy", "Anne"])
 print(names)
+# ['Bratte', 'Tom', 'Gracy', 'Anne']
 
-names.remove("Charlie")
+names.remove("Bratte")
 print(names)
+# ['Tom', 'Gracy', 'Anne']
 
-names.insert(0, "Charlie")
+names.insert(0, "Bratte")
 names.insert(1, 1984)
 print(names)
+# ['Bratte', 1984, 'Tom', 'Gracy', 'Anne']
 
-def print_help():
-    print("help function")
-
+# -----------------------------------------------------
+# Test
+# -----------------------------------------------------
 print_help()
+# ----------------------
+# help function
+# ----------------------
 
-
-def list_name():
-    for name in names:
-        show_name = "Welcome, " + str(name)
-        print(show_name)
 
 list_name()
+# ----------------------
+# Welcome, Bratte
+# Welcome, 1984
+# Welcome, Tom
+# Welcome, Gracy
+# Welcome, Anne
+# ----------------------
+
