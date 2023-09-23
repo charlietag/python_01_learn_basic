d53fffed6b42df794de4ec79a301abd308af400c
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-23 11:54:01 +0800

    classify and add comment

--- python_codes/test_class.py
+++ python_codes/test_class.py
@@ -1,55 +1,120 @@
+# -----------------------------------------------------
+# Test 1 - Basic class
+# -----------------------------------------------------
 class MyClass:
+    # class variable
     x = 5
 
-p1 = MyClass
-print(p1.x)
+# attributes x becomes instance variable in namespace (c1)
+# --- bad ---
+# c1 = MyClass
+# --- good ---
+c1 = MyClass()
 
+print(c1.x)
+# 5
 
+c1.x = "X 2"
+print(c1.x)
+# X 2
+
+c2 = MyClass()
+print(c2.x)
+# 5
+
+# attributes try_name becomes instance variable in namespace (c1)
+c1.try_name = "try Name"
+print(c1.try_name)
+# try Name
+
+print()
+print("-------------------------")
+print()
+
+# -----------------------------------------------------
+# Test 2 - class with __init__ and method
+# -----------------------------------------------------
 class Person:
+
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self._age = age
+
+    # python class built-in method
     def __str__(self):
-        return f"{self.name}({self.age})"
+        return f"{self._name}({self._age})"
+
     def my_func(self):
-        print("Hi, my name is " + self.name)
+        print("Hi, my name is " + self._name)
 
         # variable no need to declare in __init__
-        local_var_test = "test local method var"
+        local_var_test = "- test local method var -"
         print(local_var_test)
 
-p1 = Person("Charlie", 35)
-print(p1.name)
-print(p1.age)
-print(p1)
-p1.my_func()
+c1 = Person("Brad", 35)
 
-p1.name = "Welly"
-p1.age = 7
-print(p1.name)
-print(p1.age)
-print(p1)
-p1.my_func()
+print(c1._name)
+# Brad
 
+print(c1._age)
+# 35
+
+# This will call __str__
+print(c1)
+# Brad(35)
+
+c1.my_func()
+# Hi, my name is Brad
+# - test local method var -
+
+print("\n-------------------------\n")
+
+c1.name = "Anne"
+c1.age = 7
+print(c1.name)
+# Anne
+print(c1.age)
+# 7
+
+# This will call __str__
+print(c1)
+# Brad(35)
+
+c1.my_func()
+# Hi, my name is Brad
+# - test local method var -
+
+print("\n-------------------------\n")
+
+# -----------------------------------------------------
+# Test 3 - class with __init__ and method + call self.method in class
+# -----------------------------------------------------
 class Post:
+    name = "ccc"
+
     # Initial
     def __init__(self):
-        self.titles = []
-        self.print_haha()
+        self._titles = []
+        self.call_another_method()
+
     # new post
     def add_post(self, title):
-        self.titles.append(title)
+        self._titles.append(title)
+
     # delete post
     def delete_post(self, title):
-        self.titles.remove(title)
+        self._titles.remove(title)
 
-    def print_haha(self):
-        print("-------- HAHA: call from __init__ ---------")
+    def call_another_method(self):
+        print("- this line is called from __init__ -")
 
 
 a = Post()
-a.titles = [
+# - this line is called from __init__ -
+
+a._titles = [
     "asdfasdf"
 ]
 a.add_post("cccc")
-print(a.titles)
+print(a._titles)
+# ['asdfasdf', 'cccc']
