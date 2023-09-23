535219c98d31492b75d230b818d934cb5e6263c9
Charlie Hsieh <charlieofficial@gmail.com>
2023-09-23 13:23:41 +0800

    add comment

--- python_codes/test_try_except.py
+++ python_codes/test_try_except.py
@@ -1,12 +1,18 @@
-# a = "abc"
-a = 1
+a = "abc"
+# a = 1
 try:
     b = int(a)
     c = b * 2
     print(c)
+    # 2 --> if a = 1
+
 except Exception as e:
+    # if a = "abc"
+    # print err msg: invalid literal for int() with base 10: 'abc'
     print(e)
 else:
+    # if a = 1
     print("go if no error")
 finally:
+    # if a = 1, or a = "abc"
     print("go no matter error exists, or not")
